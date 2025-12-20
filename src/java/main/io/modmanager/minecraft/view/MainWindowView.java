dae365057c6bf8502ae461a4ac9979fdca1d3d3e
DevPapito <pietroeziosuzano@gmail.com>
2025-12-20 15:27:35 -0300

    Codinged image logo to main window and make new directory to invetory images also make new directory to build .jar

--- src/java/main/io/modmanager/minecraft/view/MainWindowView.java
+++ src/java/main/io/modmanager/minecraft/view/MainWindowView.java
@@ -1,6 +1,7 @@
 package io.modmanager.minecraft.view;
 
 import javax.swing.JFrame;
+import javax.swing.ImageIcon;
 
 import java.awt.BorderLayout;
 import java.awt.Toolkit;
@@ -9,15 +10,19 @@ import java.awt.Dimension;
 public class MainWindowView extends JFrame {
 
     private Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+    private ImageIcon logo;
 
     public MainWindowView(String title) {
 
         super(title);
 
+        logo = new ImageIcon(MainWindowView.class.getResource("/img/modManagerLogo.png"));
+
         this.setSize(440,560);
         this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout(0,0));
+        this.setIconImage(logo.getImage());
 
         this.centralizeScreen();
 
