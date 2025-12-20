b9f74cbac01ea2fae092cad36784d4e41486c2bc
DevPapito <pietroeziosuzano@gmail.com>
2025-12-20 15:59:46 -0300

    Maked two basic organizations JPanels/divs to main window

--- src/java/main/io/modmanager/minecraft/view/MainWindowView.java
+++ src/java/main/io/modmanager/minecraft/view/MainWindowView.java
@@ -1,6 +1,7 @@
 package io.modmanager.minecraft.view;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.ImageIcon;
 
 import java.awt.BorderLayout;
@@ -13,11 +14,21 @@ public class MainWindowView extends JFrame {
     private Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
     private ImageIcon logo;
 
+    private JPanel panelStatus;
+    private JPanel panelProperitys;
+
     public MainWindowView(String title) {
 
         super(title);
 
         logo = new ImageIcon(MainWindowView.class.getResource("/img/modManagerLogo.png"));
+
+        // JPanel +-----
+
+        panelStatus = new JPanel();
+        panelProperitys = new JPanel();
+
+        // -----+
 
         this.setSize(440,560);
         this.setResizable(false);
@@ -27,6 +38,36 @@ public class MainWindowView extends JFrame {
         this.getContentPane().setBackground(new Color(0xFFF8F7));
 
         this.centralizeScreen();
+
+        this.addJComponentsInWindow();
+
+        this.configJPanels();
+
+        this.colorJPanels();
+
+    }
+
+    public void addJComponentsInWindow() {
+
+        this.add(this.panelStatus,BorderLayout.SOUTH);
+        this.add(this.panelProperitys,BorderLayout.CENTER);
+
+    }
+
+    public void configJPanels() {
+
+        this.panelStatus.setPreferredSize(new Dimension(0,120));
+        this.panelStatus.setVisible(false);
+
+        this.panelProperitys.setPreferredSize(new Dimension(0,80));
+        this.panelProperitys.setVisible(false);
+
+    }
+
+    public void colorJPanels() {
+
+        this.panelStatus.setBackground(Color.red);
+        this.panelProperitys.setBackground(Color.blue);
 
     }
 
