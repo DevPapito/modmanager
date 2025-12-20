0d1834fe7dfef1f479edb1942e351823a71e7a96
DevPapito <pietroeziosuzano@gmail.com>
2025-12-20 14:37:38 -0300

    Coding a method of centralization of main window

--- src/java/main/io/modmanager/minecraft/view/MainWindowView.java
+++ src/java/main/io/modmanager/minecraft/view/MainWindowView.java
@@ -3,8 +3,12 @@ package io.modmanager.minecraft.view;
 import javax.swing.JFrame;
 
 import java.awt.BorderLayout;
+import java.awt.Toolkit;
+import java.awt.Dimension;
 
 public class MainWindowView extends JFrame {
+
+    private Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
 
     public MainWindowView(String title) {
 
@@ -14,6 +18,15 @@ public class MainWindowView extends JFrame {
         this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout(0,0));
+
+        this.centralizeScreen();
+
+    }
+
+    public void centralizeScreen() {
+
+        this.setLocation((this.screen.width - this.getSize().width)/2,
+                (this.screen.height - this.getSize().height)/2);
 
     }
 
